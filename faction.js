951a83164cf6c847443d96eb43f93e30c9de1b83
Aaron Franks <aaron.franks@gmail.com>
2015-10-18 09:37:13 -0700

    Initial work to support param validation.

--- faction.js
+++ faction.js
@@ -6,25 +6,24 @@ function createFaction(actionSpecs, options) {
     var types = {}
     var creators = {}
 
-    function makeActionCreator(key) {
+    function makeActionCreator(key, spec) {
         return function actionCreator(args) {
+            // TODO: validate args against spec here
+            var action = { type: key, payload: args, meta: {} }
+
             // If action argument is an error object, follow the FSA convention:
             if (args instanceof Error) {
-                return {
-                    type: key,
-                    payload: args,
-                    error: true,
-                    meta: {}
-                }
+                action.error = true
+                return action
             }
 
-            return { type: key, payload: args, meta: {} }
+            return action
         }
     }
 
     for (var key in actionSpecs) {
         types[key] = key
-        creators[key] = makeActionCreator(key)
+        creators[key] = makeActionCreator(key, actionSpecs[key])
     }
 
     return {
@@ -33,8 +32,47 @@ function createFaction(actionSpecs, options) {
     }
 }
 
+
+// Jump through hoops to get a custom Error subclass
+// See discussion on http://stackoverflow.com/q/783818
+function ActionParamError(message) {
+    this.message = message
+    var error = new Error(this.message)
+    this.stack = error.stack
+}
+ActionParamError.prototype = Object.create(Error.prototype)
+ActionParamError.prototype.name = ActionParamError.name
+ActionParamError.prototype.constructor = ActionParamError
+
+
+// Built-in param validators for action payloads
+// Each one is a function that returns the (possibly cleaned) argument value,
+// or throws a ValidationError
+var validators = {
+    string: function(x) {
+        if (typeof x === 'string') return x
+        else throw new ActionParamError('Expected "' + x + '" to be a string')
+    },
+
+    number: function(x) {
+        if (typeof x === 'number') return x
+        else throw new ActionParamError('Expected "' + x + '" to be a number')
+    },
+
+    boolean: function(x) {
+        if (typeof x === 'boolean') return x
+        else throw new ActionParamError('Expected "' + x + '" to be a boolean')
+    },
+
+    array: function(x) {
+        if (Array.isArray(x)) return x
+        else throw new ActionParamError('Expected "' + x + '" to be an array')
+    },
+}
+
 var exports = {
-    create: createFaction
+    create: createFaction,
+    validators: validators
 }
 
 
