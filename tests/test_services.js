a91458e32c52273675632ed2397c91887ef1b7f5
Aaron Franks <aaron.franks@gmail.com>
2015-11-17 20:41:33 -0800

    Add test for service rejection case.

--- tests/test_services.js
+++ tests/test_services.js
@@ -22,3 +22,22 @@ test('Async action creators with services', function(t) {
     })
 })
 
+test('Async creators with rejecting promises', function(t) {
+    var s = function(args) { return Promise.reject(args.msg) }
+    var f = faction.create({
+        REJECT_ACTION: faction.useService(s, { msg: faction.validators.string })
+    })
+
+    t.plan(5)
+    t.equal(f.types.REJECT_ACTION, 'REJECT_ACTION')
+    t.equal(typeof f.creators.REJECT_ACTION, 'function')
+
+    var action = f.creators.REJECT_ACTION({ msg: 'yo' })
+    t.equal(action.type, f.types.REJECT_ACTION)
+    t.ok(action.payload instanceof Promise)
+
+    action.payload.catch(function(val) {
+        t.equal(val, 'yo')
+    })
+})
+
