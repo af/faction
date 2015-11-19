f5fdce45f3effc55f2c443be1d7947c30e0398c7
Aaron Franks <aaron.franks@gmail.com>
2015-11-18 21:23:13 -0800

    Finally split the code into separate modules.

--- faction.js
+++ faction.js
@@ -1,9 +1,8 @@
 ;(function(global) {       // IIFE for legacy non-module usage
 'use strict'
 
-function isPromise(x) {
-    return x && typeof x.then === 'function'
-}
+var utils = require('./lib/utils')
+
 
 // Validate the arguments passed in to an action creator against the schema
 // that is declared in faction.create()
@@ -41,7 +40,7 @@ function _makeActionCreator(key, spec, service) {
         if (service) {
             // The service function *must* return a Promise, or else we throw:
             var result = service(argsHash)
-            if (!isPromise(result)) throw new Error('Service did not return a Promise')
+            if (!utils.isPromise(result)) throw new Error('Service did not return a Promise')
             action.payload = result
         } else {
             action.payload = argsHash
@@ -89,115 +88,12 @@ function useService(service, argSpecs) {
 }
 
 
-// Redux middleware for handling async actions from services
-function factionServiceMiddleware(store) {
-    var dispatch = store.dispatch
-    var oa = function(x, y) { return Object.assign({}, x, y) }
-    return function(next) {
-        return function(action) {
-            if (!isPromise(action.payload)) return next(action)
-
-            // Dispatch a "pending" version of the action, so reducers
-            // can set up any state related to loading:
-            dispatch(oa(action, {
-                payload: null,
-                meta: oa(action.meta, { isPending: true })
-            }))
-
-            action.payload.then(function(result) {
-                dispatch(oa(action, { payload: result }))
-            }).catch(function(error) {
-                dispatch(oa(action, { payload: error, error: true }))
-            })
-        }
-    }
-}
-
-
-// Jump through hoops to get a custom Error subclass
-// See discussion on http://stackoverflow.com/q/783818
-function ActionParamError(message) {
-    this.message = message
-    var error = new Error(this.message)
-    this.stack = error.stack
-}
-ActionParamError.prototype = Object.create(Error.prototype)
-ActionParamError.prototype.name = ActionParamError.name
-ActionParamError.prototype.constructor = ActionParamError
-
-
-// Built-in param validators for action payloads
-// Each one is a function that returns the (possibly cleaned) argument value,
-// or throws a ValidationError
-var validators = {
-    string: function(x) {
-        if (typeof x === 'string') return x
-        else throw new ActionParamError('Expected "' + x + '" to be a string')
-    },
-
-    number: function(x) {
-        if (typeof x === 'number' && !isNaN(x)) return x
-        else throw new ActionParamError('Expected "' + x + '" to be a number')
-    },
-
-    boolean: function(x) {
-        if (typeof x === 'boolean') return x
-        else throw new ActionParamError('Expected "' + x + '" to be a boolean')
-    },
-
-    object: function(x) {
-        if (typeof x === 'object' && x && !Array.isArray(x)) return x
-        else throw new ActionParamError('Expected "' + x + '" to be an object')
-    },
-
-    array: function(x) {
-        if (Array.isArray(x)) return x
-        else throw new ActionParamError('Expected "' + x + '" to be an array')
-    },
-}
-
-// Extra function to provide an default value for each type of built-in validator.
-// This effectively makes the field optional.
-// Example usage:
-//      v.string.withDefault('hello')
-function withDefault(defaultValue) {
-    /* jshint validthis:true */
-    var validatorFn = this
-    return function(givenValue) {
-        if (typeof givenValue === 'undefined') return defaultValue
-        else return validatorFn(givenValue)
-    }
-}
-
-// Add validation against an enum for any of the built-in validators
-// Example usage:
-//      v.number.enum([1, 2, 3], 1)
-function withEnum(optionsArray, defaultValue) {
-    /* jshint validthis:true */
-    if (!Array.isArray(optionsArray)) throw new TypeError('enum() requires an array argument')
-    var validatorFn = this
-    var hasDefault = typeof defaultValue !== 'undefined'
-    return function(givenValue) {
-        var isValid = (optionsArray.indexOf(givenValue) !== -1)
-        if (hasDefault && typeof givenValue === 'undefined') return defaultValue
-        else if (!isValid) throw new ActionParamError(givenValue + ' is not in ' +
-                                                 JSON.stringify(optionsArray))
-        else return validatorFn(givenValue)
-    }
-}
-
-for (var k in validators) {
-    validators[k].withDefault = withDefault.bind(validators[k])
-    validators[k].enum = withEnum.bind(validators[k])
-}
-
-
 var exports = {
     create: createFaction,
     useService: useService,
-    v: validators,
-    middleware: factionServiceMiddleware,
-    ActionParamError: ActionParamError
+    v: require('./lib/validators'),
+    middleware: require('./lib/middleware'),
+    ActionParamError: utils.ActionParamError
 }
 
 // Export for CommonJS, or else add a global faction variable:
